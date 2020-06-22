6af950050272ea7f7a08a86d0f293deba2e4d469
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-21 22:22:54 -0300

    Separete styles from rating component

---
+++ src/components/Rating/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+`;
+
+export const Score = styled.Text`
+  color: #9c0000;
+`;
+
+export const StarImage = styled.Image`
+  width: 10px;
+  height: 10px;
+`;
