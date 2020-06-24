d9bcc825eec7865c6d1f86d8421b76b7014bf726
chalfel <caiohalcsik@gmail.com>
2020-06-24 08:53:10 -0300

    add husky

--- src/components/ProfileImage/index.js
+++ src/components/ProfileImage/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import { Container, Image } from './styles';
+import { Container, Image } from './styles'
 
-const ProfileImage = ({ size, imgSrc, children }) =>  {
-
+const ProfileImage = ({ size, imgSrc, children }) => {
   return (
     <Container size={size}>
-      <Image size={size} source={imgSrc}/>
-      { children }
+      <Image size={size} source={imgSrc} />
+      {children}
     </Container>
-
   )
-
 }
 
-export default ProfileImage;
+export default ProfileImage
