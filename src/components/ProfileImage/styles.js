e0f96d3eb81d807087dcee98e0e9e75d62f928f5
Caio Felix <44442431+chalfel@users.noreply.github.com>
2020-06-23 23:02:21 -0300

    Update styles.js

--- src/components/ProfileImage/styles.js
+++ src/components/ProfileImage/styles.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-    height: ${props => props.size || '100px'};
-    width: ${props => props.size || '100px'};
+  height: ${props => props.size || '100px'};
+  width: ${props => props.size || '100px'};
 `;
 
 export const Image = styled.Image`
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    position: relative;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  position: relative;
 `;
