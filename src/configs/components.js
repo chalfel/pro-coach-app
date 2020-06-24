8b8a7c5a815f804299f477b8c7bea79a9669de83
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-23 23:30:48 -0300

    Create new configs folder

---
+++ src/configs/components.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Feather as Icon } from '@expo/vector-icons'
+
+export const noNavHeader = {
+  header: () => null
+}
+
+const NavBackButton = () => (
+  <Icon name="chevron-left" size={24} color="#9c0000" />
+)
+
+export const defaultNavHeader = {
+  headerBackImage: NavBackButton,
+  headerTitleStyle: {
+    color: '#9c0000'
+  },
+  headerTitleAlign: 'center'
+}
