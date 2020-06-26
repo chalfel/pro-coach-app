d2adabbe83dd5f88aa3e4700fd9c595c7c8fb0e2
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-26 20:48:33 -0300

    Create Info screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,6 +2,7 @@ import Account from './Account'
 import Checkout from './Checkout'
 import CoachService from './CoachService'
 import Home from './Home'
+import Info from './Info'
 import ServiceSearchResults from './ServiceSearchResults'
 
-export { Home, Account, Checkout, ServiceSearchResults, CoachService }
+export { Home, Account, Checkout, ServiceSearchResults, CoachService, Info }
