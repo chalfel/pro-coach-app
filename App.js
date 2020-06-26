6b3c512605a7a425e7bc2d7f3aa3f2ae41f1901b
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-25 23:45:51 -0300

    Fix indentation

--- App.js
+++ App.js
@@ -1,60 +1,59 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Feather as Icon } from '@expo/vector-icons';
-import { Home, Account, Checkout } from './src/screens';
+import React from 'react'
 
-const Tab = createBottomTabNavigator();
+import { Feather as Icon } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+
+import { Home, Account, Checkout } from './src/screens'
+
+const Tab = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={
-          ({ route }) => ({
-            tabBarIcon: ({ color, size, focused }) => {
-              const activeColor = '#000';
-              const inactiveColor = '#bbb';
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size, focused }) => {
+            const activeColor = '#000'
+            const inactiveColor = '#bbb'
 
-              if (route.name === 'Home') {
-                return (
-                  focused
-                  ? <Icon name="home" size={size} color={activeColor} />
-                  : <Icon name="home" size={size} color={inactiveColor} />
-                );
-              } else if (route.name === 'Account') {
-                return (
-                  focused
-                  ? <Icon name="user" size={size} color={activeColor} />
-                  : <Icon name="user" size={size} color={inactiveColor} />
-                );
-              } else if (route.name === 'Checkout') {
-                return (
-                  focused
-                  ? <Icon name="checkout" size={size} color={activeColor} />
-                  : <Icon name="checkout" size={size} color={inactiveColor} />
-                );
-              }
+            if (route.name === 'Home') {
+              return focused ? (
+                <Icon name="home" size={size} color={activeColor} />
+              ) : (
+                <Icon name="home" size={size} color={inactiveColor} />
+              )
+            } else if (route.name === 'Account') {
+              return focused ? (
+                <Icon name="user" size={size} color={activeColor} />
+              ) : (
+                <Icon name="user" size={size} color={inactiveColor} />
+              )
+            } else if (route.name === 'Checkout') {
+              return focused ? (
+                <Icon name="coffee" size={size} color={activeColor} />
+              ) : (
+                <Icon name="coffee" size={size} color={inactiveColor} />
+              )
             }
-          })
-        }
-        tabBarOptions={
-          {
-            style: {
-              backgroundColor: '#fff',
-              borderTopColor: '#eee'
-            },
-            activeTintColor: '#fff',
-            inactiveTintColor: '#ccc',
-            showLabel: false
           }
-        }>
+        })}
+        tabBarOptions={{
+          style: {
+            backgroundColor: '#fff',
+            borderTopColor: '#eee'
+          },
+          activeTintColor: '#fff',
+          inactiveTintColor: '#ccc',
+          showLabel: false
+        }}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Account" component={Account} />
         <Tab.Screen name="Checkout" component={Checkout} />
       </Tab.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-export default App;
+export default App
