70883a914086349c72f1fdae59ca48afcb8ff052
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-25 23:44:06 -0300

    Update screens index

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,7 @@
-import Home from './Home';
-import Account from './Account';
-import Checkout from './Checkout';
+import Account from './Account'
+import Checkout from './Checkout'
+import CoachService from './CoachService'
+import Home from './Home'
+import ServiceSearchResults from './ServiceSearchResults'
 
-export {
-  Home,
-  Account,
-  Checkout
-}
+export { Home, Account, Checkout, ServiceSearchResults, CoachService }
