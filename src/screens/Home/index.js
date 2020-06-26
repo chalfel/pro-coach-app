2441d10fc5a2f566fd81a35e4eafc50be3447da3
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-25 23:45:38 -0300

    Completed home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { View } from 'react-native'
 
 import { Feather as Icon } from '@expo/vector-icons'
 import { createStackNavigator } from '@react-navigation/stack'
+import axios from 'axios'
 
-import { Header, SearchBar, Card, Button } from '../../components'
+import { Header, Input, Card } from '../../components'
 import { defaultNavHeader, noNavHeader } from '../../configs/components'
-import { Container, Title } from './styles'
+import { apiBaseUrl, coachServicesEndpoint } from '../../configs/connection'
 
-import { Account } from '..'
+import { ServiceSearchResults, CoachService } from '..'
+
+import { Container, Title, ScrollView } from './styles'
 
 const Stack = createStackNavigator()
 
@@ -15,41 +19,125 @@ const HomeStack = () => {
   return (
     <Stack.Navigator screenOptions={defaultNavHeader}>
       <Stack.Screen name="Home" component={Home} options={noNavHeader} />
-      <Stack.Screen name="Account" component={Account} />
+      <Stack.Screen
+        name="ServiceSearchResults"
+        component={ServiceSearchResults}
+        options={{ title: 'Pesquisa' }}
+      />
+      <Stack.Screen name="CoachService" component={CoachService} />
+      {/* <Stack.Screen name="Checkout" component={Checkout} /> */}
+      {/* <Stack.Screen name="CheckoutSuccess" component={CheckoutSuccess} /> */}
     </Stack.Navigator>
   )
 }
 
 const Home = ({ navigation }) => {
+  const [topServices, setTopServices] = useState([{}])
+  const [recentServices, setRecentServices] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const placeholder = 'Encontre jogos e coaches'
+  const searchIcon = <Icon name="search" size={24} color="#aaa" />
+  const autoCorrectSearch = false
+
+  useEffect(() => {
+    const topServicesParams = {
+      params: {}
+    }
+    const recentServicesParams = {
+      params: {}
+    }
+    const topServicesReq = axios.get(
+      `${apiBaseUrl}/${coachServicesEndpoint}`,
+      topServicesParams
+    )
+    const recentServicesReq = axios.get(
+      `${apiBaseUrl}/${coachServicesEndpoint}`,
+      recentServicesParams
+    )
+
+    axios
+      .all([topServicesReq, recentServicesReq])
+      .then(
+        axios.spread((topServicesRes, recentServicesRes) => {
+          setTopServices(topServicesRes)
+          setRecentServices(recentServicesRes)
+        })
+      )
+      .catch((e) => console.log(e))
+  }, [])
+
   return (
     <Container>
-      <Button primary handleOnPress={() => navigation.navigate('Account')}>
-        Click me for testing nav
-      </Button>
       <Header />
-      <SearchBar />
+      <Input
+        placeholder={placeholder}
+        icon={searchIcon}
+        value={inputValue}
+        autoCorrect={autoCorrectSearch}
+        handleOnChange={(text) => setInputValue(text)}
+        handleOnSubmit={(text) => {
+          if (text.length > 0) {
+            setInputValue('')
+            navigation.navigate('ServiceSearchResults', {
+              text,
+              placeholder,
+              searchIcon,
+              autoCorrectSearch
+            })
+          }
+        }}
+        returnKeyType="search"
+      />
+
       <Title>
         Top coaches
         <Icon name="chevron-right" size={18} />
       </Title>
-      <Card
-        coachName="Super coach"
-        gameTitle="FIFÃO"
-        serviceTitle="Esse é meu serviço"
-        serviceDetails="Você vai curtir pra carai esse serviço!!!!!"
-        score="4.3"
-      />
+      <View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {topServices.map((service) => {
+            const { _id, user, game, name, description, rating } = service
+            return (
+              <Card
+                key={_id}
+                handleOnPress={() =>
+                  navigation.navigate('CoachService', service)
+                }
+                coachName={user}
+                gameTitle={game}
+                serviceTitle={name}
+                serviceDetails={description}
+                score={rating}
+              />
+            )
+          })}
+        </ScrollView>
+      </View>
+
       <Title>
         Últimos lançamentos
         <Icon name="chevron-right" size={18} />
       </Title>
-      <Card
-        coachName="Super coach"
-        gameTitle="FIFÃO"
-        serviceTitle="Esse é meu serviço"
-        serviceDetails="Você vai curtir pra carai esse serviço!!!!!"
-        score="4.3"
-      />
+      <View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {recentServices.map((service) => {
+            const { _id, user, game, name, description, rating } = service
+            return (
+              <Card
+                key={_id}
+                handleOnPress={() =>
+                  navigation.navigate('CoachService', service)
+                }
+                coachName={user}
+                gameTitle={game}
+                serviceTitle={name}
+                serviceDetails={description}
+                score={rating}
+              />
+            )
+          })}
+        </ScrollView>
+      </View>
     </Container>
   )
 }
