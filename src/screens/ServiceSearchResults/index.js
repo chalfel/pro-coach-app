b911e969d9ef7af29a5f2a8274d4f52624e2ea2a
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-25 23:43:22 -0300

    Add search results screen

---
+++ src/screens/ServiceSearchResults/index.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
+import { Input, Card } from '../../components'
+import { apiBaseUrl, coachServicesEndpoint } from '../../configs/connection'
+import { Container, ScrollView } from './styles'
+
+const ServiceSearchResults = ({ route }) => {
+  const { text, placeholder, searchIcon, autoCorrectSearch } = route.params
+  const [searchResults, setSearchResults] = useState([])
+  const [searchText, setSearchText] = useState(text)
+  const [inputValue, setInputValue] = useState(text)
+
+  useEffect(() => {
+    const encodedSearchText = encodeURIComponent(searchText.toLowerCase())
+    const queryParams = {
+      params: {
+        sort: 'desc(rating)',
+        contains: `username(${encodedSearchText}),gametitle(${encodedSearchText})`
+      }
+    }
+
+    axios
+      .get(`${apiBaseUrl}/${coachServicesEndpoint}`, queryParams)
+      .then(({ data }) => setSearchResults(data))
+      .catch((e) => console.log(e))
+  }, [searchText])
+
+  return (
+    <Container>
+      <Input
+        placeholder={placeholder}
+        icon={searchIcon}
+        value={inputValue}
+        autoCorrect={autoCorrectSearch}
+        handleOnSubmit={(text) => setSearchText(text)}
+        handleOnChange={(text) => setInputValue(text)}
+      />
+      <ScrollView>
+        {searchResults.map(({ _id, user, game, name, description, rating }) => (
+          <Card
+            key={_id}
+            coachName={user}
+            gameTitle={game}
+            serviceTitle={name}
+            serviceDetails={description}
+            score={rating}
+          />
+        ))}
+      </ScrollView>
+    </Container>
+  )
+}
+
+export default ServiceSearchResults
