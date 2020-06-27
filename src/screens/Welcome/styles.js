247eb554c3cc253cbf7cb076b82204e9a54af70a
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-27 18:25:01 -0300

    Add Welcome screen

---
+++ src/screens/Welcome/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #fff;
+  justify-content: flex-end;
+`
+
+export const LogoWithName = styled.Image`
+  align-self: center;
+`
+
+export const Slogan = styled.Text`
+  color: #555;
+  font-size: 18px;
+  text-align: center;
+  margin: 64px;
+`
+
+export const Link = styled.Text`
+  color: #999;
+  text-align: center;
+  margin: 16px 0 28px 0;
+  font-size: 18px;
+  font-weight: bold;
+`
