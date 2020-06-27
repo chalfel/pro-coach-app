2782c84d452f1634611794ae0916afce313ee6d3
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-27 14:39:18 -0300

    Create coach register screen

---
+++ src/screens/CoachRegister/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Platform } from 'react-native'
+
+import axios from 'axios'
+
+import { FormTitle, Input, Button } from '../../components'
+import { apiBaseUrl, coachesEndpoint } from '../../configs/connection'
+import { KeyboardAvoidingView } from './styles'
+
+const CoachService = ({ navigation }) => {
+  const [value, setValue] = useState('')
+
+  const saveCoachEmail = () => {
+    const payload = {
+      email: value
+    }
+
+    axios
+      .post(`${apiBaseUrl}/${coachesEndpoint}`, payload)
+      .then(() => {
+        navigation.navigate('CoachRegisterSuccess')
+      })
+      .catch((e) => console.log(e))
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <FormTitle>Deixe seu e-mail e nós entraremos em contato!</FormTitle>
+      <Input
+        placeholder="E-mail"
+        returnKeyType="done"
+        value={value}
+        handleOnChange={setValue}
+        handleOnSubmit={() => {}}
+      ></Input>
+      <Button primary handleOnPress={saveCoachEmail}>
+        Enviar
+      </Button>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default CoachService
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,15 @@
 import Account from './Account'
 import Checkout from './Checkout'
+import CoachRegister from './CoachRegister'
 import CoachService from './CoachService'
 import Home from './Home'
 import ServiceSearchResults from './ServiceSearchResults'
 
-export { Home, Account, Checkout, ServiceSearchResults, CoachService }
+export {
+  Home,
+  Account,
+  Checkout,
+  ServiceSearchResults,
+  CoachService,
+  CoachRegister
+}
