759228cec69e16ddc7cb480561776cd314299c32
Fernando Azevedo <azevedo.fma@gmail.com>
2020-06-26 21:24:53 -0300

    Remove icon prop for now and centralize text

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
 
 export const Container = styled.View`
   height: ${(props) => props.height || '48px'};
@@ -7,13 +7,13 @@ export const Container = styled.View`
   align-items: center;
   border-radius: 8px;
   background-color: #eee;
-  padding-left: 16px;
   margin: 16px;
-`;
+`
 
 export const TextInput = styled.TextInput`
   flex-grow: 1;
   height: ${(props) => props.height || '48px'};
-  margin: 0 0 0 16px;
+  margin: 0 0 0 0px;
   font-size: 18px;
-`;
+  text-align: center;
+`
