a3ca4d585312a5c5719e59fd826e1429919e012a
chalfel <caiohalcsik@gmail.com>
2020-06-30 09:57:53 -0300

    Create coach service folder

--- src/screens/CoachService/index.js
+++ src/screens/CoachService/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-import { TestText } from './styles'
+import {
+  ImageUpload,
+  Button,
+  Input,
+  FormSubtitle,
+  FormTitle
+} from '../../components'
+import { SafeAreaView } from './styles'
 
 const CoachService = ({ route }) => {
-  return <TestText>CoachService</TestText>
+  return (
+    <SafeAreaView>
+      <ImageUpload handleOnUpload imgSrc></ImageUpload>
+      <FormTitle>Informações pessoais</FormTitle>
+      <FormSubtitle>Informações pessoais</FormSubtitle>
+      <Input placeholder="Username" handleOnChange value />
+      <Input disabled placeholder="E-mail" handleOnChange value />
+      <Input placeholder="Discord" handleOnChange value />
+      <Input placeholder="Skype" handleOnChange value />
+      <Button primary handleOnPress>
+        Salvar alterações
+      </Button>
+      <Button handleOnPress>Logout</Button>
+    </SafeAreaView>
+  )
 }
 
 export default CoachService
--- src/screens/CoachService/styles.js
+++ src/screens/CoachService/styles.js
@@ -1,3 +1,10 @@
 import styled from 'styled-components/native'
 
-export const TestText = styled.Text``
+export const SafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  background-color: #fff;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: flex-end;
+`
