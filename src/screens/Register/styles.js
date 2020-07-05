a6a119f87fccbbdfa741da44470bd4d3bbc09f35
chalfel <caiohalcsik@gmail.com>
2020-07-05 15:56:07 -0300

    Register form cleanup and setted to the Top

--- src/screens/Register/styles.js
+++ src/screens/Register/styles.js
@@ -6,16 +6,11 @@ export const SafeAreaView = styled.KeyboardAvoidingView`
   flex: 1;
   background-color: #fff;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: flex-start;
 `
 
 export const TopContainer = styled.View`
   margin: 20px 0;
   flex-direction: column;
   justify-content: flex-start;
-`
-
-export const Container = styled.View`
-  flex-direction: column;
-  justify-content: flex-end;
 `
